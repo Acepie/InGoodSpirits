86fc56446dff4f40678811426cf39035c155a829
GerritsC <gerrits.c@husky.neu.edu>
2019-01-26 12:12:23 -0500

    we've got a vision checker.  Also I added prefabs

--- Assets/Scripts/AI/Named NPCS/TestBoy.cs
+++ Assets/Scripts/AI/Named NPCS/TestBoy.cs
@@ -6,9 +6,10 @@ public class TestBoy : NPC
 {
 
   // Use this for initialization
-  protected void Awake()
+  new protected void Awake()
   {
     base.Awake();
+
     routines.AddAction(new MoveTo(new Vector2(-3, 6), this));
     routines.AddAction(new UseElevator(this, Floor.Ground));
     routines.AddAction(new MoveTo(new Vector2(0, -4.78f), this));
---
+++ Assets/VisionChecker.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VisionChecker : MonoBehaviour
+{
+    [SerializeField]
+    private BoxCollider2D myCol;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+       if (col.tag != "Player")  { return; }
+       Player player = col.gameObject.GetComponent<Player>();
+       if (!player) {Debug.Log("Excuse me how does a player not have a player script");}
+
+       if (!player.isDiscoverable) 
+       {
+         float x_dist = col.gameObject.transform.position.x - gameObject.transform.position.x;
+         Debug.Log(x_dist);
+         if (x_dist > 0 )
+         {
+             Debug.Log("you be far");
+         }
+         else 
+         {
+             Debug.Log("They be seeing you");
+         }
+       }
+    }
+}
