b019e25f242779e47795fdfc842f20f181790d50
Ameen Radwan <therespie@gmail.com>
2019-01-26 18:01:46 -0500

    florist position again

--- Assets/Scripts/AI/Named NPCS/Florist.cs
+++ Assets/Scripts/AI/Named NPCS/Florist.cs
@@ -12,13 +12,12 @@ public class Florist : NPC
     routines.AddAction(new Wait(.1f));
     routines.AddAction(new MoveTo(elevatorManager.GetDestinationPosition(Floor.Second), this));
     routines.AddAction(new UseElevator(this, Floor.Ground));
-    routines.AddAction(new MoveTo(new Vector2(5, elevatorManager.GetDestinationPosition(Floor.Ground).y), this));
     // routines.AddAction(new TestChangeRoutine(this, testActions));
     routines.AddAction(new MoveTo(new Vector2(15, elevatorManager.GetDestinationPosition(Floor.Ground).y), this));
     routines.AddAction(new Wait(.5f));
     routines.AddAction(new MoveTo(elevatorManager.GetDestinationPosition(Floor.Ground), this));
     routines.AddAction(new UseElevator(this, Floor.Second));
-    routines.AddAction(new MoveTo(new Vector2(-9, elevatorManager.GetDestinationPosition(Floor.Second).y), this));
+    routines.AddAction(new MoveTo(new Vector2(4, elevatorManager.GetDestinationPosition(Floor.Second).y), this));
   }
 
   private void Update()
