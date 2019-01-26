da8dc5ee9fedd24af91673d93d0a571733dd7bdd
GerritsC <gerrits.c@husky.neu.edu>
2019-01-26 12:17:36 -0500

    made ameen happy

--- Assets/VisionChecker.cs
+++ Assets/VisionChecker.cs
@@ -6,22 +6,15 @@ public class VisionChecker : MonoBehaviour
 {
     [SerializeField]
     private BoxCollider2D myCol;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     void OnTriggerEnter2D(Collider2D col)
     {
        if (col.tag != "Player")  { return; }
        Player player = col.gameObject.GetComponent<Player>();
-       if (!player) {Debug.Log("Excuse me how does a player not have a player script");}
 
        if (!player.isDiscoverable) 
        {
          float x_dist = col.gameObject.transform.position.x - gameObject.transform.position.x;
-         Debug.Log(x_dist);
          if (x_dist > 0 )
          {
              Debug.Log("you be far");
