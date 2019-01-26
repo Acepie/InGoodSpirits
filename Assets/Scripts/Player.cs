d7e2bcf234941d0ad04c9b662968ff72a3a1be25
chosencharacters <chosencharacter@gmail.com>
2019-01-26 12:16:52 -0500

    Acceleration Based Movement
    Added Acceleration Based Movement, all old velocity code has been removed

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,7 +5,13 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
 
-  public float speed = 5f;
+  public float initSpeed = .25f;
+  public float speedGain = 1.01f;
+  public float speedLoss = 1.02f;
+  public float maxSpeed = 7f;
+  public float controlLoss = 0;
+  private int tick = 0;
+  private Rect bounds = new Rect(-12f, 10f, 18, 16); 
   Rigidbody2D rb2d;
   PlayerInteractable interactableHoveringOver;
 
@@ -17,6 +23,7 @@ public class Player : MonoBehaviour
   // Update is called once per frame
   private void Update()
   {
+    tick++;
     OnHover();
     Move();
     if (interactableHoveringOver != null && Input.GetKeyDown(KeyCode.Space))
@@ -27,27 +34,77 @@ public class Player : MonoBehaviour
 
   private void Move()
   {
-    Vector2 velocity = new Vector2(0, 0);
-    if (Input.GetKey("left") || Input.GetKey("a"))
-    {
-      velocity += new Vector2(-speed, 0);
+    Vector2 velocity = rb2d.velocity;
+    bool up = Input.GetKey("up") || Input.GetKey("w");
+    bool down = Input.GetKey("down") || Input.GetKey("s");
+    bool left = Input.GetKey("left") || Input.GetKey("a");
+    bool right = Input.GetKey("right") || Input.GetKey("d");
+
+    //Bounds Checks
+    bool bup =  rb2d.position.y > bounds.y;
+    bool bdown = rb2d.position.y < bounds.y - bounds.height;
+    bool bleft = rb2d.position.x < bounds.x;
+    bool bright = rb2d.position.x > bounds.x + bounds.width;
+
+    if(controlLoss <= 0 && (bup || bdown || bleft || bright)){
+      if(bup) rb2d.position += new Vector2(0, 0.1f);
+      if(bdown) rb2d.position += new Vector2(0, -0.1f);
+      if(bleft) rb2d.position += new Vector2(-0.1f, 0);
+      if(bright) rb2d.position += new Vector2(0.1f, 0);
+      velocity.x = -velocity.x;
+      velocity.y = -velocity.y;
+      controlLoss = 15;
     }
 
-    if (Input.GetKey("right") || Input.GetKey("d"))
-    {
-      velocity += new Vector2(speed, 0);
+    if(controlLoss > 0){
+      up = down = left = right = false;
+      controlLoss--;
     }
 
-    if (Input.GetKey("up") || Input.GetKey("w"))
+    if (up)
     {
-      velocity += new Vector2(0, speed);
+      if(velocity.y < .25) velocity.y = initSpeed;
+      velocity.y = velocity.y * speedGain;
     }
 
-    if (Input.GetKey("down") || Input.GetKey("s"))
-    {
-      velocity += new Vector2(0, -speed);
+    if (down)
+    {      
+      if(velocity.y > -.25) velocity.y = -initSpeed;
+      velocity.y = velocity.y * speedGain;
     }
+
+    if (left)
+    {
+      if(velocity.x > -.25) velocity.x = -initSpeed;
+      velocity.x = velocity.x * speedGain;
+    }
+
+    if (right)
+    {      
+      if(velocity.x < .25) velocity.x = initSpeed;
+      velocity.x = velocity.x * speedGain;
+    }
+
+    //Drag code, is lessened when controlLoss > 0 so drag isn't as intense
+    if(controlLoss%2==0){
+      if(!left && !right && tick % 3 == 0){
+        velocity.x = velocity.x/speedLoss;
+      } 
+      if(!up && !down && tick % 3 == 0){
+        velocity.y = velocity.y/speedLoss;
+      }
+    }
+
+    velocity = maxSpeedLock(velocity);
     rb2d.velocity = velocity;
+  }
+
+  private Vector2 maxSpeedLock(Vector2 velocity){
+    if(velocity.x > maxSpeed) velocity.x = maxSpeed;
+    if(velocity.x < -maxSpeed) velocity.x = -maxSpeed;
+    if(velocity.y > maxSpeed) velocity.y = maxSpeed;
+    if(velocity.y < -maxSpeed) velocity.y = -maxSpeed;
+    return velocity;
   }
 
   private void OnHover()
