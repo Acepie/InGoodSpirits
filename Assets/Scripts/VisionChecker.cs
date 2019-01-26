845487cc2e86237e4a2966fb79f6b5f1c6b380a1
GerritsC <gerrits.c@husky.neu.edu>
2019-01-26 14:43:06 -0500

    added happy face when npc's are friends.

--- Assets/Scripts/VisionChecker.cs
+++ Assets/Scripts/VisionChecker.cs
@@ -12,24 +12,46 @@ public class VisionChecker : MonoBehaviour
     private Sprite suspicious_sprite;
     [SerializeField]
     private NPC parent_Script;
+    [SerializeField]
+    private Sprite happyFace;
+
+    //Checks for player specific detection.  set to true if you want the npc to detect 
+    private bool is_playerDetectable = true;
 
     void OnTriggerStay2D(Collider2D col)
     {
-       if (col.tag != "Player")  { return; }
-       Player player = col.gameObject.GetComponent<Player>();
-
-       if (!player.isDiscoverable)
+       if (col.tag.Equals("Player"))  
        {
-         float x_dist = col.gameObject.transform.position.x - gameObject.transform.position.x;
-         if (x_dist > 0 )
-         {
-            parent_Script.SetEmote(suspicious_sprite, true);
-         }
-         else 
-         {
-            parent_Script.SetEmote(alerted_sprite, true);
-         }
+        Player player = col.gameObject.GetComponent<Player>();
+
+        if (is_playerDetectable && !player.isDiscoverable)
+        {
+            float x_dist = col.gameObject.transform.position.x - gameObject.transform.position.x;
+            if (x_dist > 0 )
+            {
+                parent_Script.SetEmote(suspicious_sprite, true);
+            }
+            else 
+            {
+                parent_Script.SetEmote(alerted_sprite, true);
+            }
+        }
        }
+       else  if (col.tag.Equals("NPC"))
+       {
+           //We're checking if we're friendly
+           NPC otherNPC = col.gameObject.GetComponent<NPC>();
+
+           if (parent_Script.AreWeFriends(otherNPC))
+           {
+               parent_Script.SetEmote(happyFace);
+           }
+       }
+    }
+
+    public void SetPlayerDetectable(bool i_detectable)
+    {
+        is_playerDetectable = i_detectable;
     }
 
     void OnTriggerExit2D(Collider2D col)
