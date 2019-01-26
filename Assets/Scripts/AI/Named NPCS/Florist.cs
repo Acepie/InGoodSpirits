1d610f35a87b278ec314fa0d545ba672dd3006b7
Ameen Radwan <therespie@gmail.com>
2019-01-26 13:14:22 -0500

    added florist basic loop

--- Assets/Scripts/AI/Named NPCS/Florist.cs
+++ Assets/Scripts/AI/Named NPCS/Florist.cs
@@ -2,15 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Florist : MonoBehaviour {
+public class Florist : NPC
+{
+  ElevatorManager elevatorManager;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+  new protected void Awake()
+  {
+    base.Awake();
+    elevatorManager = GameObject.FindGameObjectWithTag("Elevator Manager").GetComponent<ElevatorManager>();
+    Loop();
+    Loop();
+  }
+
+  void Loop()
+  {
+    routines.AddAction(new Wait(.1f));
+    routines.AddAction(new MoveTo(elevatorManager.GetDestinationPosition(Floor.Second), this));
+    routines.AddAction(new UseElevator(this, Floor.Ground));
+    routines.AddAction(new MoveTo(new Vector2(15, elevatorManager.GetDestinationPosition(Floor.Ground).y), this));
+    routines.AddAction(new Wait(.5f));
+    routines.AddAction(new MoveTo(elevatorManager.GetDestinationPosition(Floor.Ground), this));
+    routines.AddAction(new UseElevator(this, Floor.Second));
+    routines.AddAction(new MoveTo(new Vector2(-9, elevatorManager.GetDestinationPosition(Floor.Second).y), this));
+  }
 }
