75633e64af7425fd58c69ea0499f90494b09a3d9
GerritsC <gerrits.c@husky.neu.edu>
2019-01-26 13:41:27 -0500

    added emote slot and alerting/suspcious Icons

--- Assets/Scripts/VisionChecker.cs
+++ Assets/Scripts/VisionChecker.cs
@@ -6,23 +6,37 @@ public class VisionChecker : MonoBehaviour
 {
     [SerializeField]
     private BoxCollider2D myCol;
+    [SerializeField]
+    private Sprite alerted_sprite;
+    [SerializeField]
+    private Sprite suspicious_sprite;
+    [SerializeField]
+    private NPC parent_Script;
 
-    void OnTriggerEnter2D(Collider2D col)
+    void OnTriggerStay2D(Collider2D col)
     {
        if (col.tag != "Player")  { return; }
        Player player = col.gameObject.GetComponent<Player>();
 
-       if (!player.isDiscoverable) 
+       if (!player.isDiscoverable)
        {
          float x_dist = col.gameObject.transform.position.x - gameObject.transform.position.x;
          if (x_dist > 0 )
          {
+             parent_Script.SetEmote(suspicious_sprite, true);
              Debug.Log("you be far");
          }
          else 
          {
+             parent_Script.SetEmote(alerted_sprite, true);
              Debug.Log("They be seeing you");
          }
        }
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        Debug.Log("exit");
+        parent_Script.SetEmoteVisibility(false);
     }
 }
