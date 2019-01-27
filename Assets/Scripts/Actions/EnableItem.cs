f15677b7649f9c753627c6555917cc3603866958
Ameen Radwan <therespie@gmail.com>
2019-01-27 14:58:49 -0500

    only adds cookies if old ones picked up

--- Assets/Scripts/Actions/EnableItem.cs
+++ Assets/Scripts/Actions/EnableItem.cs
@@ -4,38 +4,43 @@ using UnityEngine;
 
 public class EnableItem : IAction
 {
-    public GameObject[] itemToEnable;
-    public GameObject prefabToCreate;
-    Vector3 locationToMakeItem;
+  public GameObject[] itemToEnable;
+  public GameObject prefabToCreate;
+  Vector3 locationToMakeItem;
+  public GameObject lastCreated;
 
   public EnableItem(GameObject[] g)
   {
     itemToEnable = g;
   }
 
-    public EnableItem(GameObject g, Vector3 pos)
-    {
-        prefabToCreate = g;
-        locationToMakeItem = pos;
-        itemToEnable = new GameObject[0];
-    }
+  public EnableItem(GameObject g, Vector3 pos)
+  {
+    prefabToCreate = g;
+    locationToMakeItem = pos;
+    itemToEnable = new GameObject[0];
+  }
 
   public IEnumerator DoAction()
   {
-        if(itemToEnable.Length > 0)
-        {
-            foreach (GameObject item in itemToEnable)
-            {
-                item.GetComponent<Collider2D>().enabled = true;
-                item.GetComponent<SpriteRenderer>().enabled = true;
-                SoundManager.PlaySound(item.GetComponent<PickupInteractable>().clipToPlay);
-            }
-        }
-        else
-        {
-            GameObject newItem = Object.Instantiate(prefabToCreate) as GameObject;
-            newItem.transform.position = locationToMakeItem;
-        }
+    if (itemToEnable.Length > 0)
+    {
+      foreach (GameObject item in itemToEnable)
+      {
+        item.GetComponent<Collider2D>().enabled = true;
+        item.GetComponent<SpriteRenderer>().enabled = true;
+        SoundManager.PlaySound(item.GetComponent<PickupInteractable>().clipToPlay);
+      }
+    }
+    else
+    {
+      PickupInteractable pui = lastCreated?.GetComponent<PickupInteractable>();
+      if (pui == null || pui.pickedUp)
+      {
+        lastCreated = Object.Instantiate(prefabToCreate) as GameObject;
+        lastCreated.transform.position = locationToMakeItem;
+      }
+    }
 
     yield return null;
   }
